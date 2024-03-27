903b7fd0b37a302ee7df1de743ccb5aa82be4c7b
loenan <laurent.istin@free.fr>
2024-03-27 09:18:43 +0100

    Add & reorganize tests with won balls up to 3

--- src/test/java/com/loenan/kata/tennis/TennisGameScoreTest.java
+++ src/test/java/com/loenan/kata/tennis/TennisGameScoreTest.java
@@ -1,62 +1,66 @@
 package com.loenan.kata.tennis;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
+import static java.util.Collections.emptyList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class TennisGameScoreTest {
 
     private TennisGameScore tennisGameScore = new TennisGameScore();
 
-    @Test
-    void shouldReturn15vs0Message_whenPlayerAWinFirstBall() {
-        // given
-        String gameBallWinners = "A";
-
-        // when
+    @ParameterizedTest
+    @MethodSource("provideScoreArguments")
+    void shouldReturnExpectedMessages_givenBallWinners(String gameBallWinners, List<String> expectedMessages) {
         List<String> scoreMessages = tennisGameScore.getScoreMessages(gameBallWinners);
 
-        // then
-        assertThat(scoreMessages).containsExactly("Player A : 15 / Player B : 0");
+        assertThat(scoreMessages).containsExactlyElementsOf(expectedMessages);
     }
 
-    @Test
-    void shouldReturn0vs15Message_whenPlayerBWinFirstBall() {
-        // given
-        String gameBallWinners = "B";
-
-        // when
-        List<String> scoreMessages = tennisGameScore.getScoreMessages(gameBallWinners);
-
-        // then
-        assertThat(scoreMessages).containsExactly("Player A : 0 / Player B : 15");
-    }
-
-    @Test
-    void shouldReturnNoMessage_whenNoBallPlayed() {
-        // given
-        String gameBallWinners = "";
-
-        // when
-        List<String> scoreMessages = tennisGameScore.getScoreMessages(gameBallWinners);
-
-        // then
-        assertThat(scoreMessages).isEmpty();
-    }
-
-    @Test
-    void shouldReturnNoMessage_whenNullPassed() {
-        // given
-        String gameBallWinners = null;
-
-        // when
-        List<String> scoreMessages = tennisGameScore.getScoreMessages(gameBallWinners);
-
-        // then
-        assertThat(scoreMessages).isEmpty();
+    public static Stream<Arguments> provideScoreArguments() {
+        return Stream.of(
+            arguments("A", List.of("Player A : 15 / Player B : 0")),
+            arguments("B", List.of("Player A : 0 / Player B : 15")),
+            arguments("", emptyList()),
+            arguments(null, emptyList()),
+            arguments("AB", List.of(
+                "Player A : 15 / Player B : 0",
+                "Player A : 15 / Player B : 15"
+            )),
+            arguments("AA", List.of(
+                "Player A : 15 / Player B : 0",
+                "Player A : 30 / Player B : 0"
+            )),
+            arguments("AAA", List.of(
+                "Player A : 15 / Player B : 0",
+                "Player A : 30 / Player B : 0",
+                "Player A : 40 / Player B : 0"
+            )),
+            arguments("BB", List.of(
+                "Player A : 0 / Player B : 15",
+                "Player A : 0 / Player B : 30"
+            )),
+            arguments("BBB", List.of(
+                "Player A : 0 / Player B : 15",
+                "Player A : 0 / Player B : 30",
+                "Player A : 0 / Player B : 40"
+            )),
+            arguments("ABABA", List.of(
+                "Player A : 15 / Player B : 0",
+                "Player A : 15 / Player B : 15",
+                "Player A : 30 / Player B : 15",
+                "Player A : 30 / Player B : 30",
+                "Player A : 40 / Player B : 30"
+            ))
+        );
     }
 
     @Test
@@ -68,20 +72,5 @@ class TennisGameScoreTest {
         assertThatThrownBy(() -> tennisGameScore.getScoreMessages(gameBallWinners))
             // then
             .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    void shouldReturn15vs15Message_whenAWinBallThenBWinBall() {
-        // given
-        String gameBallWinners = "AB";
-
-        // when
-        List<String> scoreMessages = tennisGameScore.getScoreMessages(gameBallWinners);
-
-        // then
-        assertThat(scoreMessages).containsExactly(
-            "Player A : 15 / Player B : 0",
-            "Player A : 15 / Player B : 15"
-        );
     }
 }
